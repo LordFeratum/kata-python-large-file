d731983135bb226838cda6fc084687aeb8d2dba4
Quique Piña <lord.feratum@gmail.com>
2017-11-27 22:52:32 +0100

    multiprocessing

--- kata/main.py
+++ kata/main.py
@@ -1,37 +1,78 @@
-import codecs
-
+from multiprocessing import Process, JoinableQueue, Queue, cpu_count
+# from codecs import iterdecode
+# from itertools import islice, groupby
 from urllib.request import urlopen
 
 from settings import settings
 
 
-def download_csv(url, encoding='utf-8'):
-    def _iter(response):
-        line = response.readline()
-        while line:
-            yield line
-            line = response.readline()
+class CSVProcessor(Process):
+    def __init__(self, task_queue, result_queue):
+        super().__init__()
+        self._task_queue = task_queue
+        self._result_queue = result_queue
+        self._result = dict(rows=0, avg=0)
 
+    def run(self):
+        while True:
+            task = self._task_queue.get()
+            if task.is_eof():
+                break
+
+            self._result['rows'] += 1
+            self._result['avg'] += task()
+            self._task_queue.task_done()
+
+        self._result_queue.put(self._result)
+        return
+
+
+class Task:
+    def __init__(self, row, is_eof=False):
+        self._row = row.decode('utf-8')
+        self._is_eof = is_eof
+
+    def __call__(self):
+        return 1
+
+    def is_eof(self):
+        return self._is_eof
+
+    def __str__(self):
+        return self._row
+
+
+def process_csv(url, tasks, encoding='utf-8'):
     response = urlopen(url)
-    return codecs.iterdecode(_iter(response), encoding)
-
-
-def process_csv(iterdata):
-    total_rows = 0
-    for row, datum in enumerate(iterdata):
-        total_rows = row
-
-    return {
-        'total_rows': total_rows,
-        'average_tip_amount': 0
-    }
+    line = response.readline()
+    while line:
+        tasks.put(Task(line))
+        line = response.readline()
 
 
 def main():
     url = settings['amazon']['url']
-    csv = download_csv(url)
-    result = process_csv(csv)
-    print(result)
+
+    tasks = JoinableQueue()
+    results = Queue()
+
+    consumers = [
+        CSVProcessor(tasks, results)
+        for _ in range(cpu_count() * 2)
+    ]
+    for consumer in consumers:
+        consumer.start()
+
+    process_csv(url, tasks)
+
+    for _ in consumers:
+        tasks.put(Task(None, is_eof=True))
+
+    tasks.join()
+
+    while True:
+        result = results.get()
+        print("RESULT: ", result)
 
 
 if __name__ == '__main__':
